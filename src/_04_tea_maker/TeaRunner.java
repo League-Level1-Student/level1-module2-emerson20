b2e89f894fd97dc2a9c7c4816fe185c8a97ea393
League Student <league@iMac-16.attlocal.net>
2019-12-29 17:24:48 -0800

    Minion and TeaRunner

--- src/_04_tea_maker/TeaRunner.java
+++ src/_04_tea_maker/TeaRunner.java
@@ -1,5 +1,12 @@
-package _04_tea_maker;
+ package _04_tea_maker;
 
 public class TeaRunner {
-
+public static void main(String[] args) {
+	Cup c1 = new Cup();
+	Kettle kat = new Kettle();
+	TeaBag tea = new TeaBag(TeaBag.MINT);
+	kat.getWater();
+	kat.boil();
+	c1.makeTea(tea, kat.getWater());
+}
 }
