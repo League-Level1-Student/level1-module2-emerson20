f363c783b2269cab060a5c6d712a71d7fc612d1d
League Student <league@iMac-16.attlocal.net>
2020-01-19 17:23:09 -0800

    Minion and MinionTest

--- src/_06_minion/MinionTest.java
+++ src/_06_minion/MinionTest.java
@@ -20,10 +20,7 @@ This is because there is no Minion class in the package.
 2.
  So to make these tests pass, you will first need to create a Minion class with the member variables below:
 
-  			private String name; 
-  			private int eyes; 
-  			private String color; 
-  			private String master;
+  			
 
  3.
   Next, create a constructor, and getters and setters for all the member variables of the Minion class. 
