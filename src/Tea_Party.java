c21e0ec4eed9a32c385b32d11849d926fd1e1398
League Student <league@iMac-16.attlocal.net>
2020-01-26 17:25:10 -0800

    Minion, MinionTest, Tea Party, TeaPartyTest, and Tea_Party

---
+++ src/Tea_Party.java
@@ -0,0 +1,4 @@
+
+public class Tea_Party {
+
+}
--- src/_06_minion/Minion.java
+++ src/_06_minion/Minion.java
@@ -10,5 +10,23 @@ public class Minion {
 		this.eyes=eyes;
 		this.color=color;
 		this.master=master;
+	
+	}public String getName() {
+		return this.name;
+	}public void setName(String name) {
+		this.name=name;
+	}public int getEyes() {
+		return this.eyes;
+	}public void setEyes(int eyes) {
+		this.eyes=eyes;
+	}public String getColor() {
+		return this.color;
+	}public void setColor(String color) {
+		this.color=color;
+	}public String getMaster() {
+		return this.master;
+	}public void setMaster(String master) {
+		this.master=master;
 	}
 }
+                                                                        
