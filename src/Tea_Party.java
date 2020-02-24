f10e30d607ec125bf3f50e77693593909ca5bc1a
League Student <league@iMac-16.attlocal.net>
2020-02-23 17:19:16 -0800

    Tea_Party and TeaPartyTest

--- src/Tea_Party.java
+++ src/Tea_Party.java
@@ -1,4 +1,22 @@
 
 public class Tea_Party {
-
+public String welcome(String name, boolean isWoman, boolean isKnighted) {
+    String h =  "Hello";
+    
+    if (isWoman) {
+		return h+ " Ms. " + name;
+	}else if (isWoman == false && isKnighted == false) {
+		return h+ " Mr. " + name;
+	}else if (isWoman == false && isKnighted) {
+		 return h+ " Sir " + name;
+	}return null;
+//    if (isKnighted == false && isWoman) {
+//		System.out.println("Hello Ms. Austen");
+//	}else if (isKnighted && isWoman == false) {
+//		System.out.println("Hello Mr. Orwell");
+//	}else {
+//		System.out.println("Hello Sir Isaac Newton");
+//	}
+    }
 }
+                                                                                                   
